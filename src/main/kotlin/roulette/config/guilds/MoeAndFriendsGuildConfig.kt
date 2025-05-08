080ad2a08e7c7644a273f85eb10db3c2c52c10c8
マシュ <jarek13705@gmail.com>
2025-05-08 16:16:24 +0900

    Update timeout strings for Moe and Friends (#5)

--- src/main/kotlin/roulette/config/guilds/MoeAndFriendsGuildConfig.kt
+++ src/main/kotlin/roulette/config/guilds/MoeAndFriendsGuildConfig.kt
@@ -54,14 +54,29 @@ internal val MOE_AND_FRIENDS_GUILD_CONFIG = object : GuildConfiguration {
 
     override val responseDelay = UIntRange(1u, 5u)
 
-    // Strings for a timeout that should be applied onto a user.
-    private val TIMEOUT_AFFECTED_STRINGS = setOf(
-        { target: String, duration: String -> "Hitdad sees and Hitdad strikes. $target is muted for $duration." },
-        { target: String, duration: String -> "Daughter protection. It's to die for. $target is muted for $duration." },
+    // Strings for a timeout that should be applied onto a user by themselves.
+    private val TIMEOUT_SELF_AFFECTED_STRINGS = setOf(
+        { target: String, duration: String -> "Hitdad sees and Hitdad strikes $target with a flying briefcase. They will come to their senses in $duration." },
         { target: String, duration: String -> "What's the most you ever lost on a coin toss? $target wagered $duration." },
         { target: String, duration: String -> "Reverend Waluigi has deemed $target a sinner. Their penance: A vow of silence for $duration." },
         { target: String, duration: String -> "General Waluigi has sent $target's squad on a secret mission to spread the Word of Wah. They will be radio silent for $duration." },
-        { target: String, duration: String -> "Eyeless dogs are prowling around $target's house. They'll go away after $duration." }
+        { target: String, duration: String -> "Eyeless dogs are prowling around $target's house. They'll go away after $duration." },
+        { target: String, duration: String -> "Hitdad topdecks Exodia, the Forbidden One, sending $target to the Shadow Realm for $duration." },
+        { target: String, duration: String -> "Hitdad does his best Luigi impression, shooting $target in the back and ending their reign of tyranny. They will respawn in $duration." },
+        { target: String, duration: String -> "$target ate a burger and got food poisoning. They will recover in $duration." },
+        { target: String, duration: String -> "Truck-kun blew through the intersection, yeeting $target into the metaverse. They will be able to return in $duration." },
+        { target: String, duration: String -> "Hitdad throws $target into the nearest TV. They will be rescued in $duration." },
+        { target: String, duration: String -> "The moon falls on $target, crushing them and the land around. Time will rewind in $duration." },
+        { target: String, duration: String -> "Hitdad tiger drops $target, negating all damage and leaving them unconscious. They will wake up in $duration." },
+    )
+
+    // Strings for a timeout that should be applied onto a user by a moderator.
+    private val TIMEOUT_OTHER_AFFECTED_STRINGS = setOf(
+        { target: String, duration: String -> "Hitdad goes hunting for the Toshi ping. $target is hit in the crossfire and will return in $duration." },
+        { target: String, duration: String -> "Hitdad puts his Sheriff hat on and snipes $target in the head. They will return in $duration." },
+        { target: String, duration: String -> "The M&F Sheriff's Department has issued a warrant for $target's arrest. They will be released in $duration." },
+        { target: String, duration: String -> "The moderation team has decided to mod-abuse $target for $duration." },
+        { target: String, duration: String -> "The ghost of Chae whacks $target with the banhammer. Because she's a ghost, it only mutes them for $duration." },
     )
 
     // Strings for a timeout that should be not applied onto a user.
@@ -69,11 +84,21 @@ internal val MOE_AND_FRIENDS_GUILD_CONFIG = object : GuildConfiguration {
         "The love of Moe's life, $target is not deserving of a $duration mute."
     }
 
-    private val TIMEOUT_RESPONDER = Action.Timeout.Responder { target, outcome, _, duration ->
+    private val TIMEOUT_RESPONDER = Action.Timeout.Responder { target, outcome, initiator, duration ->
         when {
-            outcome == Action.Outcome.AFFECTED ->
-                TIMEOUT_AFFECTED_STRINGS.random().invoke(target.effectiveName, duration.toHumanString())
+            // User self roll.
+            outcome == Action.Outcome.AFFECTED && target == initiator ->
+                TIMEOUT_SELF_AFFECTED_STRINGS.random().invoke(target.effectiveName, duration.toHumanString())
 
+            // Moderator roll by mod/Anivanilla.
+            outcome == Action.Outcome.AFFECTED && initiator.idLong == 29966380407980034 ->
+                "Ani readies her gun and fires indiscriminately, hitting ${target.effectiveName} and knocking them out for ${duration.toHumanString()}."
+
+            // Rolled by a moderator.
+            outcome == Action.Outcome.AFFECTED && target != initiator ->
+                TIMEOUT_OTHER_AFFECTED_STRINGS.random().invoke(target.effectiveName, duration.toHumanString())
+
+            // Target is protected against rolls.
             else -> TIMEOUT_UNAFFECTED_STRINGS.random().invoke(target.effectiveName, duration.toHumanString())
         }
     }
